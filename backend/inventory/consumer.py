4a02c115e84469aee0c575bd9c87c1ef49637898
ravinduf <ravinduf@rocketmail.com>
2022-07-02 10:51:37 +0530

    added redis streams and created consumer.py to listen to the stream and reduce the qty from product

---
+++ backend/inventory/consumer.py
@@ -0,0 +1,31 @@
+from ast import Try
+from main import redis, Product
+import time
+
+key = 'order_completed'
+group = 'inventory-group'
+
+try:
+    redis.xgroup_create(key, group)
+except:
+    print('Group already exists!')
+
+while True:
+    try:
+        results = redis.xreadgroup(group, key, {key: '>'}, None)
+
+        print(results)
+
+        if results != []:
+            for result in results:
+                obj = result[1][0][1]
+                print(obj)
+                product = Product.get(obj['product_id'])
+
+                print(product)
+                product.quantity = product.quantity - int(obj['quantity'])
+                product.save()
+
+    except Exception as e:
+        print(str(e))
+    time.sleep(1)
